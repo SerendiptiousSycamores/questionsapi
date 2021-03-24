6fd720459b66ce332943ad8eb131d7ca4af9ce0e
jackzhen1996 <jackzhen999@gmail.com>
2021-03-24 00:31:17 -0700

    implemented docker containers for db and server

--- schema.sql
+++ schema.sql
@@ -32,3 +32,18 @@ CREATE TABLE photos (
   answer_id integer references answers(id) ON DELETE CASCADE
 
 );
+
+CREATE UNIQUE INDEX sortproductid on products(id);
+CREATE INDEX helpful_index_desc on questions(question_helpfulness DESC);
+CREATE INDEX question_not_reported on questions(id) where reported is not true;
+CREATE INDEX questionDate on questions(question_date DESC);
+CREATE INDEX questionId on questions(id);
+CREATE INDEX search_by_productid on questions(product_id) INCLUDE (id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported);
+
+CREATE INDEX answerDate on answers(created_at DESC);
+CREATE INDEX answerId on answers(id);
+CREATE INDEX answersHelpful_index on answers(helpfulness DESC);
+CREATE INDEX search_by_questionid ON answers(question_id) INCLUDE (id, answer_body, created_at, answerer_name, answerer_email, helpfulness, reported);
+
+CREATE UNIQUE INDEX photoId on photos(id);
+CREATE INDEX search_by_answerid on photos(answer_id) INCLUDE (id,url);
--- src/controllers/qaController.js
+++ src/controllers/qaController.js
@@ -4,16 +4,17 @@ exports.getQuestions = (req,res) => {
   const responseObj = {};
   const {product_id, count} = req.query;
   db.query(
-    'select array_to_json(array_agg(t)) from (select *,(select json_object_agg(d.id, d) from (select *,( select array_to_json(array_agg(row_to_json(c))) from ( select id, url from photos where photos.answer_id = answers.id )c ) as photos from answers where answers.question_id = questions.id and answers.reported = false ) d ) as answers from questions where questions.product_id  = $1 and questions.reported = false order by question_helpfulness DESC limit $2 ) t', [product_id, count || 5])
+    'select array_to_json(array_agg(t)) from (select id, question_body, question_date, asker_name, question_helpfulness, reported,(select json_object_agg(d.id, d) from (select id,answer_body,created_at,answerer_name, helpfulness, reported, ( select array_to_json(array_agg(row_to_json(c))) from ( select id, url from photos where photos.answer_id = answers.id )c ) as photos from answers where answers.question_id = questions.id and answers.reported = false ) d ) as answers from questions where questions.product_id  = $1 and questions.reported = false order by question_helpfulness DESC limit $2 ) t', [product_id, count || 5])
   .then(result=>{
     responseObj.product_id = product_id;
     responseObj.results = result.rows[0]['array_to_json'];
-    res.send(responseObj)
+    res.send(responseObj);
   })
   .catch(err=>{throw err})
 }
 
 exports.postQuestion =  (req,res) => {
+  console.log(req.body)
   const {body, name, email, product_id} = req.body;
   db.query('INSERT INTO questions(question_body, question_date, asker_name, product_id, asker_email) values($1,$2,$3,$4,$5)',
   [body, new Date().toJSON(), name, product_id, email])
@@ -23,13 +24,7 @@ exports.postQuestion =  (req,res) => {
 
 exports.upvoteQuestion =  (req,res) => {
   const {question_id} = req.params;
-  console.log(question_id)
-  db.query('SELECT question_helpfulness from questions where id = $1 ',
-  [question_id])
-    .then(result=>{
-      console.log(result.rows[0].question_helpfulness)
-      return db.query('UPDATE questions SET question_helpfulness = $1 WHERE id = $2', [result.rows[0].question_helpfulness+1, question_id]);
-    })
+  db.query('update questions set question_helpfulness = question_helpfulness + 1 where id = $1',[question_id])
     .then(finished=>res.send(`Upvoted a question with question ID: ${question_id}`))
     .catch(err=>{throw err});
 }
@@ -47,7 +42,7 @@ exports.getAnswers =  (req,res) => {
   const {question_id, count, page} = req.query;
   const responseObj = {question:question_id,count: count || 5 ,page:0};
   db.query(
-    'select array_to_json(array_agg(row_to_json(t))) from (select *, (select array_to_json(array_agg(row_to_json(d))) from ( select id, url from photos where photos.answer_id  = answers.id )d ) as photos  from answers where answers.question_id = $1 and answers.reported = false order by helpfulness DESC limit $2)t', [question_id, responseObj.count])
+    'select array_to_json(array_agg(row_to_json(t))) from (select id,answer_body,created_at,answerer_name, helpfulness, reported, (select array_to_json(array_agg(row_to_json(d))) from ( select id, url from photos where photos.answer_id  = answers.id )d ) as photos  from answers where answers.question_id = $1 and answers.reported = false order by helpfulness DESC limit $2)t', [question_id, responseObj.count])
       .then(result=>{
         responseObj.results = result.rows[0]['array_to_json']
         res.send(responseObj);
@@ -58,8 +53,7 @@ exports.getAnswers =  (req,res) => {
 }
 
 exports.postAnswer =  (req,res) => {
-  const {body, name, email, photos} = req.body;
-  const {question_id} = req.params;
+  const {body, name, email, photos,question_id} = req.body;
   db.query('INSERT INTO answers (answer_body, answerer_name, answerer_email, question_id, created_at) values($1, $2, $3, $4, $5)', [body, name, email, question_id, new Date().toJSON()])
     .then(success=>{
       if (photos.length !== 0) {
@@ -81,12 +75,8 @@ exports.postAnswer =  (req,res) => {
 
 exports.upvoteAnswer =  (req,res) => {
   const {answer_id} = req.params;
-  db.query('SELECT helpfulness from answers where id = $1 ',
-  [answer_id])
-    .then(result=>{
-      return db.query('UPDATE answers SET helpfulness = $1 WHERE id = $2', [result.rows[0].helpfulness+1, answer_id]);
-    })
-    .then(finished=>res.send(`Upvoted an answer with answer id: ${answer_id}`))
+  db.query('update answers set helpfulness = helpfulness + 1 where id = $1',[answer_id])
+    .then(finished=>res.send(`Upvoted an answer with answer ID: ${answer_id}`))
     .catch(err=>{throw err});
 
 }
@@ -98,14 +88,5 @@ exports.reportAnswer =  (req,res) => {
       res.send(`Reported question with answer id ${answer_id}`);
     })
     .catch(err=>{throw err});
-
-}
-
-exports.test = (req,res) => {
-  db.query('select array_to_json(array_agg(row_to_json(t)))from (select id,url from photos where answer_id = 10) t;')
-  .then(result=>{
-    res.send(result);
-  })
-  .catch(err=>{throw err});
 
 }
